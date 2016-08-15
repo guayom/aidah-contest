5805c5b1e7c5a216440d2dd1b4d38f1035c2bde4
Guayo Mena <guayo.mena@gmail.com>
2016-08-14 23:48:40 -0600

    Added footer and changed navigation template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,33 +15,35 @@
 
 //= require_tree .
 
-$(window).on("load resize",function(e) {
-  var more = document.getElementById("js-centered-more");
-
+//Navigation
+$(window).resize(function() {
+  var more = document.getElementById("js-navigation-more");
   if ($(more).length > 0) {
     var windowWidth = $(window).width();
     var moreLeftSideToPageLeftSide = $(more).offset().left;
     var moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
 
     if (moreLeftSideToPageRightSide < 330) {
-      $("#js-centered-more .submenu .submenu").removeClass("fly-out-right");
-      $("#js-centered-more .submenu .submenu").addClass("fly-out-left");
+      $("#js-navigation-more .submenu .submenu").removeClass("fly-out-right");
+      $("#js-navigation-more .submenu .submenu").addClass("fly-out-left");
     }
 
     if (moreLeftSideToPageRightSide > 330) {
-      $("#js-centered-more .submenu .submenu").removeClass("fly-out-left");
-      $("#js-centered-more .submenu .submenu").addClass("fly-out-right");
+      $("#js-navigation-more .submenu .submenu").removeClass("fly-out-left");
+      $("#js-navigation-more .submenu .submenu").addClass("fly-out-right");
     }
   }
+});
 
-  var menuToggle = $("#js-centered-navigation-mobile-menu").unbind();
-  $("#js-centered-navigation-menu").removeClass("show");
+$(document).ready(function() {
+  var menuToggle = $("#js-mobile-menu").unbind();
+  $("#js-navigation-menu").removeClass("show");
 
   menuToggle.on("click", function(e) {
     e.preventDefault();
-    $("#js-centered-navigation-menu").slideToggle(function(){
-      if($("#js-centered-navigation-menu").is(":hidden")) {
-        $("#js-centered-navigation-menu").removeAttr("style");
+    $("#js-navigation-menu").slideToggle(function(){
+      if($("#js-navigation-menu").is(":hidden")) {
+        $("#js-navigation-menu").removeAttr("style");
       }
     });
   });
