8f5505af61cc26c1ddaa9b0222e5e99fd37046f3
Guayo Mena <guayo.mena@gmail.com>
2016-08-16 22:24:36 -0600

    Added voted_for? method in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_one :participant, dependent: :destroy
+
+  def voted_for?(category)
+    Vote.where(:category_id => category.id, :user_id => self.id)
+  end
 end
