d9982e19d06c78c1d3a57f84e9b09016e29df3f8
Guayo Mena <guayo.mena@gmail.com>
2017-06-03 12:33:44 -0600

    Limpieza de proyecto

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,3 @@
 Rails.application.routes.draw do
-
-  resources :participant_ratings
-  resources :votes
-  resources :categories
-
-  get '/participante/:id', to: 'participants#show', as: 'participante'
-  resources :participants do
-    member do
-      post 'upvote'
-    end
-    resources :category do
-      resources :participant_ratings
-    end
-  end
-
-  resources :groups
-  resources :parameters
-  resources :juries
-
-  get '/user/:id', to: 'users#show'
-
   devise_for :users, controllers: { confirmations: 'confirmations' }
 end
