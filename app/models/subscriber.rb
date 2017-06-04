b727cd27ec831a402a935bf777e3674cbdd3fafd
Guayo Mena <guayo.mena@gmail.com>
2017-06-04 12:10:47 -0600

    Added active recod validation for Subscriber

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,2 +1,8 @@
 class Subscriber < ApplicationRecord
+  validates :email, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :name, presence: true
+  validates :tel, presence: true
+  validates :category, presence: true
+  validates :country, presence: true
 end
