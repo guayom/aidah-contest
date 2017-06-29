a34485331c7da6ab96a1a8664e7337d70e178995
Guayo Mena <guayo.mena@gmail.com>
2017-06-28 21:28:59 -0600

    Removed routs for localization

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :subscribers, only: [:new, :create]
-  #get '/dancers', to: 'subscribers#dancer'
-  root 'subscribers#new'
-
-  get '/:locale' => 'subscribers#new'
-
-  devise_for :users, controllers: { confirmations: 'confirmations' }
-
-  scope "/:locale", locale: /en|es/ do
-    get "/pages/:id" => 'high_voltage/pages#show', :as => :page, :format => false
-    get '/dancers', to: 'subscribers#dancer'
-  end
+  get '/dancers', to: 'subscribers#dancer'
+  
+  # root 'subscribers#new'
+  #
+  # get '/:locale' => 'subscribers#new'
+  #
+  # devise_for :users, controllers: { confirmations: 'confirmations' }
+  #
+  # scope "/:locale", locale: /en|es/ do
+  #   get "/pages/:id" => 'high_voltage/pages#show', :as => :page, :format => false
+  #   get '/dancers', to: 'subscribers#dancer'
+  # end
 
 end
