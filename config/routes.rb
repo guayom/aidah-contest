bfb3e78d3bbefc879468686642af899873854b57
guayom <guayo.mena@gmail.com>
2017-07-03 09:08:27 -0600

    Revert "trying to avoid redirect"
    This reverts commit 6d7fc33cac2192c997b6e648714e91dc6f16777b.

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,9 @@ Rails.application.routes.draw do
   resources :subscribers, only: [:new, :create]
   get '/dancers', to: 'subscribers#dancer'
   get "/paginas/:id" => 'high_voltage/pages#show'
-  get '/', to: 'subscribers#new'
   #get 'pages/:id' => 'high_voltage/pages#show', id: 'home'
 
-  #root 'subscribers#new'
+  root 'subscribers#new'
 
   devise_for :users, controllers: { confirmations: 'confirmations' }
 
